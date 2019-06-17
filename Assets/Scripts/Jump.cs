cbb0caf8e333649926429c96f4a2309e79483e08
ZA139 <a1399677960@foxmail.com>
2019-06-18 00:07:29 +0800

    Initial commit

---
+++ Assets/Scripts/Jump.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jump : MonoBehaviour
+{
+    private float verticalVelocity;
+    private CharacterController controller;
+    private float gravity = 17.0f;
+    private float jumpForce = 10.0f;
+
+    // Use this for initialization
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (controller.isGrounded)
+        {
+            verticalVelocity = -gravity * Time.deltaTime;
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                verticalVelocity = jumpForce;
+            }
+        }
+        else
+        {
+            verticalVelocity -= gravity * Time.deltaTime;
+        }
+        Vector3 moveVector = new Vector3(0, verticalVelocity, 0);
+        controller.Move(moveVector * Time.deltaTime);
+    }
+
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,311 @@
+using UnityEngine;
+using System.Collections;
+
+[AddComponentMenu("Game/Player")]
+public class Player : MonoBehaviour
+{
+    //各种特殊弹药的数量
+    public int ShockedAmmo = 5;
+    public int OnFireAmmo = 5;
+    public int DamAmmo = 5;
+    public int ElectricAmmo = 5;
+    public int NormalAmmo = 5;
+    //创建特殊弹药类型
+    public Stack SpecialMag = new Stack();
+    //枚举弹药类型
+    enum SpecialAmmo
+    {
+        ShcokedAmmo,
+        OnFireAmmo,
+        DamAmmo,
+        ElectricAmmo,
+        NormalAmmo
+    };
+    public Transform m_transform;
+
+    public bool SpecialMagOn = false;
+
+    // 角色控制器组件
+    CharacterController m_ch;
+
+    // 角色移动速度
+    float m_movSpeed = 3.0f;
+
+    // 重力
+    float m_gravity = 2.0f;
+
+    // 摄像机Transform
+    Transform m_camTransform;
+
+    // 摄像机旋转角度
+    Vector3 m_camRot;
+
+    // 摄像机高度(即表示主角的身高)
+    float m_camHeight = 1.4f;
+
+    // 生命值
+    public int m_life = 5;
+
+    // 枪口transform
+    Transform m_muzzlepoint;
+
+    // 射击时，射线能射到的碰撞层
+    public LayerMask m_layer;
+
+    // 射中目标后的粒子效果
+    public Transform m_fx;
+
+    // 射击音效
+    public AudioClip m_audio;
+
+    // 射击间隔时间计时器
+    float m_shootTimer = 0;
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+        m_transform = this.transform;
+        // 获取角色控制器组件
+        m_ch = this.GetComponent<CharacterController>();
+
+        // 获取摄像机
+        m_camTransform = Camera.main.transform;
+
+        // 设置摄像机初始位置
+        //Vector3 pos = m_transform.position;
+        //pos.y += m_camHeight;
+        //m_camTransform.position = pos;
+        m_camTransform.position = m_transform.TransformPoint(0, m_camHeight, 0);
+
+        // 设置摄像机的旋转方向与主角一致
+        m_camTransform.rotation = m_transform.rotation;
+        m_camRot = m_camTransform.eulerAngles;
+        //锁定鼠标
+        Screen.lockCursor = true;
+
+        // 查找射击点
+        m_muzzlepoint = m_camTransform.Find("M16/weapon/muzzlepoint").transform;
+
+        SpecialMag.Clear();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // 生命值为0，GG
+        if (m_life <= 0)
+            return;
+
+        Control();
+    }
+
+    void Control()
+    {
+
+        //获取鼠标移动距离
+        float rh = Input.GetAxis("Mouse X");
+        float rv = Input.GetAxis("Mouse Y");
+
+        // 旋转摄像机
+        m_camRot.x -= rv;
+        m_camRot.y += rh;
+        m_camTransform.eulerAngles = m_camRot;
+
+        // 使主角的面向方向与摄像机一致
+        Vector3 camrot = m_camTransform.eulerAngles;
+        camrot.x = 0; camrot.z = 0;
+        m_transform.eulerAngles = camrot;
+
+        // 定义3个值控制移动
+        float xm = 0, ym = 0, zm = 0;
+
+        // 重力运动
+        ym -= m_gravity * Time.deltaTime;
+
+        // 上下左右运动
+        if (Input.GetKey(KeyCode.W))
+        {
+            zm += m_movSpeed * Time.deltaTime;
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            zm -= m_movSpeed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            xm -= m_movSpeed * Time.deltaTime;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            xm += m_movSpeed * Time.deltaTime;
+        }
+
+        // 更新射击间隔时间
+        m_shootTimer -= Time.deltaTime;
+
+        //特殊弹药压入
+        if (Input.GetKeyDown(KeyCode.Alpha1) && (ShockedAmmo != 0))
+        {
+            SpecialMag.Push(SpecialAmmo.ShcokedAmmo);//麻痹弹药
+            ShockedAmmo--;
+            GameManager.Instance.SetAmmo(0);
+            Debug.Log("ShockedAMMO");
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2) && (OnFireAmmo != 0))
+        {
+            SpecialMag.Push(SpecialAmmo.OnFireAmmo);//火炎弹药
+            OnFireAmmo--;
+            GameManager.Instance.SetAmmo(0);
+            Debug.Log("OnFireAMMO");
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3) && (DamAmmo != 0))
+        {
+            SpecialMag.Push(SpecialAmmo.DamAmmo);//达姆弹
+            DamAmmo--;
+            GameManager.Instance.SetAmmo(0);
+            Debug.Log("DamAMMO");
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha4) && (ElectricAmmo != 0))
+        {
+            SpecialMag.Push(SpecialAmmo.ElectricAmmo);//电击弹药
+            ElectricAmmo--;
+            GameManager.Instance.SetAmmo(0);
+            Debug.Log("ElectricAMMO");
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha5))
+        {
+            SpecialMag.Push(SpecialAmmo.NormalAmmo);
+            Debug.Log("NormalAMMO");
+            GameManager.Instance.SetAmmo(0);
+        }
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            if (!SpecialMagOn)
+                SpecialMagOn = true;
+            else
+                SpecialMagOn = false;
+            GameManager.Instance.SetAmmo(0);
+
+        }
+        // 鼠标左键射击
+        if (Input.GetMouseButton(0) && m_shootTimer <= 0)
+        {
+            if (SpecialMagOn)
+            {
+                if (SpecialMag.Count != 0)
+                    shoot();
+            }
+            else
+                shoot();
+        }
+
+
+        // 使用角色控制器提供的Move函数进行移动 它会自动检测碰撞
+        m_ch.Move(m_transform.TransformDirection(new Vector3(xm, ym, zm)));
+
+        // 更新摄像机位置（始终与Player一致）
+        //Vector3 pos = m_transform.position;
+        //pos.y += m_camHeight;
+        //m_camTransform.position = pos;
+        m_camTransform.position = m_transform.TransformPoint(0, m_camHeight, 0);
+    }
+
+    // 在编辑器中为主角显示一个图标
+    void OnDrawGizmos()
+    {
+        Gizmos.DrawIcon(this.transform.position, "Spawn.tif");
+    }
+
+
+    // 受到伤害
+    public void OnDamage(int damage)
+    {
+        m_life -= damage;
+
+        // 更新UI
+        GameManager.Instance.SetLife(m_life);
+
+        // 取消锁定鼠标光标
+        if (m_life <= 0)
+            Screen.lockCursor = false;
+    }
+
+    void shoot()
+    {
+        if (SpecialMagOn)
+        {
+            //SpecialAmmoShooted = SpecialMag.Pop();
+        }
+        m_shootTimer = 0.1f;
+        this.GetComponent<AudioSource>().PlayOneShot(m_audio);
+        // 减少弹药，更新弹药UI
+
+        // RaycastHit用来保存射线的探测结果
+        RaycastHit info;
+
+        // 从muzzlepoint的位置，向摄像机面向的正方向射出一根射线
+        // 射线只能与m_layer所指定的层碰撞
+        bool hit = Physics.Raycast(m_muzzlepoint.position, m_camTransform.TransformDirection(Vector3.forward), out info, 100, m_layer);
+        if (hit)
+        {
+            // 如果射中了Tag为enemy的游戏体
+            if (info.transform.tag.CompareTo("enemy") == 0)
+            {
+                Enemy enemy = info.transform.GetComponent<Enemy>();
+                if (!SpecialMagOn)
+                    enemy.OnDamage(1);
+                else
+                {
+                    if (SpecialMag.Count != 0)
+                    {
+                        switch (SpecialMag.Pop())
+                        {
+                            case SpecialAmmo.ShcokedAmmo:
+                                enemy.Shocked = true;
+                                enemy.OnDamage(3);
+                                break;
+                            case SpecialAmmo.OnFireAmmo:
+                                enemy.OnFire = true;
+                                enemy.OnDamage(1);
+                                break;
+                            case SpecialAmmo.ElectricAmmo://连锁功能，可通过rigibody组件来实现
+                                enemy.OnDamage(10);
+                                break;
+                            case SpecialAmmo.DamAmmo:
+                                enemy.OnDamage(30);
+                                break;
+                            case SpecialAmmo.NormalAmmo:
+                                enemy.OnDamage(1);
+                                break;
+                        }
+                    }
+                }
+                GameManager.Instance.SetAmmo(1);
+            }
+            else
+            {
+                if (SpecialMagOn)
+                {
+                    SpecialMag.Pop();
+                }
+                GameManager.Instance.SetAmmo(1);
+            }
+            // 在射中的地方释放一个粒子效果
+            Instantiate(m_fx, info.point, info.transform.rotation);
+        }
+
+
+    }
+}
+/*
+ printf("xiang mu shijian");
+ freopen("项目.txt","w",stdout);
+ if(需要）
+ {
+ freopen("项目.txt","r",stdin);
+ */
